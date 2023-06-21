b7c25042eb244835742f0e5e651a2ac1b893aac3
Lanqing Yuan <yuanlq@uchicago.edu>
2023-06-21 00:14:08 -0500

    more info

--- setup.py
+++ setup.py
@@ -6,9 +6,29 @@ with open('requirements.txt') as f:
         r.split('/')[-1] if r.startswith('git+') else r
         for r in f.read().splitlines()]
 
+# Get the long description from the README file
+with open('README.md') as file:
+    readme = file.read()
+
 setuptools.setup(
     name='saltax',
+    author='Lanqing Yuan',
+    description='Salting analysis framework for XENONnT.',
+    long_description=readme,
     version='0.0.0',
     install_requires=requires,
+    setup_requires=['pytest-runner'],
+    python_requires=">=3.8",
     packages=setuptools.find_packages(),
+    classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'License :: MIT License',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Intended Audience :: Science/Research',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Physics',
+    ],
 )
