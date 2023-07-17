8f5a8a82488b93c1ff6dbed53a6e21814c9a1b38
Lanqing Yuan <yuanlq@uchicago.edu>
2023-07-17 16:22:37 -0500

    generate_times

--- saltax/instructions/generator.py
+++ saltax/instructions/generator.py
@@ -4,7 +4,12 @@ import wfsim
 from packaging import version
 
 
-def generate_vertex(r_range=(0, 66.4), z_range=(-148.15, 0), size=1):
+SALT_TIME_INTERVAL = 1e7 # in unit of ns. The number should be way bigger then full drift time
+Z_RANGE = (-148.15, 0)
+R_RANGE = (0, 66.4)
+
+
+def generate_vertex(r_range=R_RANGE, z_range=Z_RANGE, size=1):
     """
     Generate a random vertex in the TPC volume.
     :param r_range: (r_min, r_max) in cm
@@ -23,18 +28,42 @@ def generate_vertex(r_range=(0, 66.4), z_range=(-148.15, 0), size=1):
 
     return x, y, z
 
+def generate_times(start_time, end_time, size=None, rate=1e9/SALT_TIME_INTERVAL, time_mode='uniform'):
+    """
+    Generate an array of event times in the given time range.
+    :param start_time: start time in ns
+    :param end_time: end time in ns
+    :param size: rough number of events to generate
+    :param rate: rate of events in Hz
+    :param time_mode: 'uniform' or 'realistic'
+    :return: array of event times in ns
+    """
+    total_time_ns = end_time - start_time
+    estimated_size = int(total_time_ns * rate / 1e9)
 
-def generate_times(rate, size, timemode):
-    assert timemode in ["realistic", "uniform"], "timemode must be either 'realistic' or 'uniform'"
+    assert time_mode in ["realistic", "uniform"], "timemode must be either 'realistic' or 'uniform'"
+
     # Generating event times from exponential
-    if timemode == "realistic":
-        dt = np.random.exponential(1 / rate, size=size - 1)
+    # This one doesn't work for salting!!!
+    if time_mode == "realistic":
+        dt = np.random.exponential(1 / rate, size=estimated_size-1)
         times = np.append([1.0], 1.0 + dt.cumsum()) * 1e9
         times = times.round().astype(np.int64)
-        return times
+        times += start_time
+
     # Generating event times from uniform
-    elif timemode == "uniform":
-        dt = (1 / rate) * np.ones(size - 1)
+    elif time_mode == "uniform":
+        dt = (1 / rate) * np.ones(estimated_size-1)
         times = np.append([1.0], 1.0 + dt.cumsum()) * 1e9
         times = times.round().astype(np.int64)
+        times += start_time
+    
+    # Removing events that are too close to the start or end of the run
+    times = times[times < end_time-1/rate]
+    times = times[times > start_time+1/rate]
+
+    if size is None:
         return times
+    elif size >= 0:
+        return times[:min(int(size), len(times))]
+    
