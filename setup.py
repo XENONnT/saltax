eef394318c45daee53a3f7fdb8e801e3635dc2f9
Lanqing Yuan <yuanlq@uchicago.edu>
2023-07-19 15:49:31 -0500

    twix before setup

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ with open('requirements.txt') as f:
         r.split('/')[-1] if r.startswith('git+') else r
         for r in f.read().splitlines()]
 
+# Manually add cutax which is private
+requires += ['cutax @ git+ssh://git@github.com/XENONnT/cutax.git@v1.15.1']
+
 # Get the long description from the README file
 with open('README.md') as file:
     readme = file.read()
@@ -18,13 +21,13 @@ setuptools.setup(
     version='0.0.0',
     install_requires=requires,
     setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
     python_requires=">=3.9",
     packages=setuptools.find_packages(),
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'License :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Intended Audience :: Science/Research',
