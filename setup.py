966bb132effcf335286339095263a56e89240cfb
Lanqing Yuan <yuanlq@uchicago.edu>
2023-07-20 16:36:12 -0500

    format

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     version='0.0.0',
     install_requires=requires,
     setup_requires=['pytest-runner'],
-    tests_require=requires+['pytest', "hypothesis", "boltons"],
+    tests_require=requires+['pytest',"hypothesis","boltons"],
     python_requires=">=3.9",
     packages=setuptools.find_packages(),
     classifiers=[
