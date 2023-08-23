4afe0a687d2e409f5db17433bb7ade4335295c3e
Lanqing Yuan <yuanlq@uchicago.edu>
2023-08-22 23:29:14 -0500

    remove unused import

--- saltax/plugins/s_raw_records.py
+++ saltax/plugins/s_raw_records.py
@@ -2,7 +2,6 @@ import strax
 import numpy as np
 import wfsim
 import logging
-import cutax
 from wfsim import extra_truth_dtype_per_pmt
 from immutabledict import immutabledict
 from wfsim.strax_interface import SimulatorPlugin, instruction_dtype, rand_instructions, instruction_from_csv
