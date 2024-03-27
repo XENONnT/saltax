c77d80b82d051ccb680983cba5de37d885835ae2
Lanqing Yuan <yuanlq@uchicago.edu>
2024-03-27 05:44:25 -0500

    redefined matching

--- saltax/match/match.py
+++ saltax/match/match.py
@@ -181,6 +181,7 @@ def _pair_salt_to_simu_events(truth, match, events_simu, events_salt):
     
     return ind_salt_matched_to_simu, ind_simu_matched_to_truth, truth_filtered, match_filtered
 
+
 def match_events_deprecated(truth, match, events_simu, events_salt):
     """
     Match salted events to simulation events. This function has been deprecated because it cannot track
@@ -200,6 +201,86 @@ def match_events_deprecated(truth, match, events_simu, events_salt):
     events_simu_matched_to_salt = events_simu_matched_to_truth[ind_salt_matched_to_simu>=0]
 
     return events_salt_matched_to_simu, events_simu_matched_to_salt
+
+
+def match_events(events_simu, events_salt):
+    """
+    Match salted events to simulation events based on event or S1-S2 timing, without checking 
+    simulation truth. 
+    The procedures are:
+        1. Filter out events_simu which is missing S1. Then the rest events_simu is like 'truth'
+           without ambience interference. Those events_simu are called events_simu_filtered.
+        2. Find indices of events_salt whose time range overlaps with events_simu_filtered: 
+           ind_salt_event_found. Those events_salt[ind_salt_event_found] are called 
+           events_salt_event_found.
+        3. Find indcies of events_simu_filtered whose time range overlaps with events_salt_event_found: 
+           ind_simu_event_found. Those events_simu_filtered[ind_simu_event_found] are called 
+           events_simu_event_found. 
+        4. Find indcies of events_salt whose S1 time range overlaps with events_simu_filtered's S1 time 
+           range: ind_salt_s1_found. Those events_salt[ind_salt_s1_found] are called events_salt_s1_found.
+        5. Find indcies of events_simu_filtered whose S1 time range overlaps with events_salt_s1_found:
+           ind_simu_s1_found. Those events_simu_filtered[ind_simu_s1_found] are called events_simu_s1_found.
+        6. Find indcies of events_salt whose S2 time range overlaps with events_simu_filtered's S2 time 
+           range: ind_salt_s2_found. Those events_salt[ind_salt_s2_found] are called events_salt_s2_found.
+        7. Find indcies of events_simu_filtered whose S2 time range overlaps with events_salt_s2_found:
+           ind_simu_s2_found. Those events_simu_filtered[ind_simu_s2_found] are called events_simu_s2_found.
+    :param events_simu: event_info from wfsim
+    :param events_salt: event_info from saltax
+    :return: events_simu_filtered, 
+             ind_salt_event_found, ind_simu_event_found, ind_simu_event_lost, ind_simu_event_split,
+             ind_salt_s1_found, ind_simu_s1_found, 
+             ind_salt_s2_found, ind_simu_s2_found
+    """
+    # Step 1.
+    # Filter out events_simu which is missing S1
+    events_simu_filtered = events_simu[events_simu['s1_time']>=0]
+
+    # Step 2 & 3.
+    # Find indices of events_salt whose time range overlaps with events_simu_filtered
+    event_touching_windows = strax.touching_windows(events_salt, events_simu_filtered)
+    event_windows_length = event_touching_windows[:,1] - event_touching_windows[:,0]
+    # If windows_length is not 1, the sprinkled event is either split or not found
+    mask_simu_event_found = event_windows_length == 1
+    mask_simu_event_lost  = event_windows_length == 0
+    mask_simu_event_split = event_windows_length >= 2
+    ind_simu_event_found = np.where(mask_simu_event_found)[0]
+    ind_simu_event_lost  = np.where(mask_simu_event_lost)[0]
+    ind_simu_event_split = np.where(mask_simu_event_split)[0]
+    # Find indcies of events_salt whose event time range overlaps with events_simu_filtered
+    ind_salt_event_found = event_touching_windows[mask_simu_event_found][:,0]
+
+    # Step 4 & 5.
+    # Assumed s1 times has been sorted! It should be an safe assumption because their events are sorted.
+    s1_touching_windows = strax._touching_windows(
+        events_salt["s1_time"],
+        events_salt["s1_endtime"],
+        events_simu_filtered["s1_time"],
+        events_simu_filtered["s1_endtime"],
+    )
+    s1_windows_length = s1_touching_windows[:,1] - s1_touching_windows[:,0]
+    mask_simu_s1_found = s1_windows_length == 1
+    ind_simu_s1_found = np.where(mask_simu_s1_found)[0]
+    ind_salt_s1_found = s1_touching_windows[mask_simu_s1_found][:,0]
+
+    # Step 6 & 7.
+    # Assumed s2 times has been sorted! It should be an safe assumption because their events are sorted.
+    s2_touching_windows = strax._touching_windows(
+        events_salt["s2_time"],
+        events_salt["s2_endtime"],
+        events_simu_filtered["s2_time"],
+        events_simu_filtered["s2_endtime"],
+    )
+    s2_windows_length = s2_touching_windows[:,1] - s2_touching_windows[:,0]
+    mask_simu_s2_found = s2_windows_length == 1
+    ind_simu_s2_found = np.where(mask_simu_s2_found)[0]
+    ind_salt_s2_found = s2_touching_windows[mask_simu_s2_found][:,0]
+
+    return (
+        events_simu_filtered,
+        ind_salt_event_found, ind_simu_event_found, ind_simu_event_lost, ind_simu_event_split,
+        ind_salt_s1_found, ind_simu_s1_found,
+        ind_salt_s2_found, ind_simu_s2_found
+    )
 
 
 def match_peaks(truth, match, peaks_simu, peaks_salt):
