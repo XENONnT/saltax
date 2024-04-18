6800f92176830a8182d8f47d8813756dfae65d5a
Lanqing Yuan <yuanlq@uchicago.edu>
2024-04-18 13:51:09 -0500

    renamed records and peaklets

--- saltax/plugins/__init__.py
+++ saltax/plugins/__init__.py
@@ -1,8 +1,8 @@
 from . import s_raw_records
 from .s_raw_records import *
 
-from . import s_records
-from .s_records import *
+from . import records
+from .records import *
 
-from . import s_peaklets
-from .s_peaklets import *
+from . import peaklets
+from .peaklets import *
