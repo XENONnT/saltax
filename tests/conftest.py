9aeb1a9a37004d9e6fce101768000fc02c5fa8ba
dachengx <dx2227@columbia.edu>
2025-07-01 13:43:23 -0500

    Bug fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import os
 import pytest
 
 
-@pytest.fixture(autouse=True)
+# session scope is needed when running locally to ensure
+# that the environment variable is set before any import
+@pytest.fixture(autouse=True, scope="session")
 def set_env():
     os.environ["NUMBA_DISABLE_JIT"] = "1"
