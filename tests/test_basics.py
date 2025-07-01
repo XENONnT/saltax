d898506f1ec8d67f367daf1d8ef298c05f27a8dd
dachengx <dx2227@columbia.edu>
2025-07-01 12:52:44 -0500

    Finalize

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -5,7 +5,8 @@ import saltax
 
 if os.environ["NUMBA_DISABLE_JIT"] != "1":
     raise RuntimeError(
-        "NUMBA_DISABLE_JIT must be set to 1 to run saltax tests. "
+        "NUMBA_DISABLE_JIT must be set to 1 to run saltax tests. Because for unknown reasons, "
+        "errors of channel number out of range are not raised in numba JIT compiled code. "
         "Please run `export NUMBA_DISABLE_JIT=1` in your terminal before running the tests."
     )
 
