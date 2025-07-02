b5d736a6d37b156f547adcc8a125ee660fccee24
dachengx <dx2227@columbia.edu>
2025-07-02 10:41:00 -0500

    Simplify

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,6 @@ import os
 import pytest
 
 
-# session scope is needed when running locally to ensure
-# that the environment variable is set before any import
-@pytest.fixture(autouse=True, scope="session")
-def set_env():
-    os.environ["NUMBA_DISABLE_JIT"] = "1"
-
-
 @pytest.fixture(autouse=True, scope="function")
 def check_env():
     if os.environ["NUMBA_DISABLE_JIT"] != "1":
