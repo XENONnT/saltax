9b7c5d1d1fc4a1247ff86997052f8ca0eb73d4bd
dachengx <dx2227@columbia.edu>
2025-09-27 14:48:06 -0500

    Bug fix

--- saltax/test_utils.py
+++ saltax/test_utils.py
@@ -23,6 +23,7 @@ def get_test_context(saltax_mode):
     """Get a test context for the given saltax mode."""
     st = saltax.contexts.sxenonnt(
         saltax_mode=saltax_mode,
+        simulation_config="sr1_dev",
         # lowest possible version to modify as less as possible
         corrections_version="global_v10",
         output_folder="./strax_test_data",
